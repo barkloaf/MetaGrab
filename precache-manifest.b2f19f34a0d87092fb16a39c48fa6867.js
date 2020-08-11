287182b9738c9606f4b421c34841290c1ee2709c
barkloaf <46911272+barkloaf@users.noreply.github.com>
2020-08-10 20:53:07 -0700

    Updates

---
+++ precache-manifest.b2f19f34a0d87092fb16a39c48fa6867.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14719a10c5bab2b2db3d494a7e17e85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68db78ee6b25d237127",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7daeb166bb37c454527",
+    "url": "/static/css/main.4bf9e66c.chunk.css"
+  },
+  {
+    "revision": "c68db78ee6b25d237127",
+    "url": "/static/js/2.6ecaea1c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6ecaea1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7daeb166bb37c454527",
+    "url": "/static/js/main.428aa724.chunk.js"
+  },
+  {
+    "revision": "51b7cc1440fdb2cc3410",
+    "url": "/static/js/runtime-main.3efa5d51.js"
+  }
+]);
